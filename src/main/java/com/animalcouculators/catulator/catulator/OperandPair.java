8ce53571fff62ed58c73286e148cd64445e4d4da
SamCartledge <cartledgesam15@gmail.com>
2018-08-17 13:14:56 -0700

    enitial check-in

---
+++ src/main/java/com/animalcouculators/catulator/catulator/OperandPair.java
@@ -0,0 +1,13 @@
+package com.animalcouculators.catulator.catulator;
+
+public class OperandPair {
+    public String FirstOperand;
+    public String SecondOperand;
+    public String OperatorType;
+
+    public OperandPair(){
+        this.FirstOperand = "";
+        this.SecondOperand = "";
+        this.OperatorType = "";
+    }
+}
